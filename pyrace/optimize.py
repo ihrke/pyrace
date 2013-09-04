64987f961d3679ef448d6e6cf79e6c488ade18c8
Matthias Mittner <mittner@uva.nl>
2013-09-04 11:54:59 +0200

    DE/rand/1/bin with multiprocessing
    and Rastrigin test

--- pyrace/optimize.py
+++ pyrace/optimize.py
@@ -5,6 +5,7 @@ import multiprocessing as mp
 import copy
 
 
+
 def opt_func_deviance( x, mod, data, trace ):
     xp=mod.untrans(x)
     mod.set_params(xp)
@@ -23,6 +24,21 @@ def opt_func_deviance_precalc( x, mod, data, trace ):
     if trace=='full':
         print score, xp
     return score
+
+
+def _cont_chunks(l, n):
+    """
+    Divide list l in n chunks (preserve order)
+    """
+    ll=[]
+    c=int(np.ceil(len(l)/float(n)))
+    for i in range(0, len(l), c):
+        ll.append(l[i:i+c])
+    return ll
+    
+
+def _flatten(l):
+    return [item for sublist in l for item in sublist]
 
 
 def de_rand_1_bin( pop_ini, objective, objargs=(), F=.5, CR=0, gen_max=1000, save_stats=None ):
@@ -92,7 +108,76 @@ def de_rand_1_bin( pop_ini, objective, objargs=(), F=.5, CR=0, gen_max=1000, sav
 
     return cpop, score, stats if save_stats!=None else None
 
-#def de_rand_1_bin_mp( pop_ini, objective, objargs ):
+
+def _eval_pop( candidates, objective, objargs ):
+    res=[objective(cand, *objargs) for cand in candidates]
+    return res
+
+def de_rand_1_bin_mp( pop_ini, objective, objargs=(), F=.5, CR=0, gen_max=1000, save_stats=None, pool=None, ncpu=4 ):
+    """
+    same as de_rand_1_bin except that each population is evaluated in parallel.
+
+    pool : multiprocessing.Pool instance or None (own pool is created)
+    ncpu : number of cores (not used if pool!=None)
+    """
+    if pool==None:
+        pool=mp.Pool(ncpu)
+    else:
+        ncpu=pool._processes
+    
+    stopcrit=False
+    npop=len(pop_ini)
+    D=len(pop_ini[0])
+
+    # initial evaluation
+    candchunks=_cont_chunks(pop_ini, ncpu)
+    jobs=[pool.apply_async( _eval_pop, (x, objective,objargs) ) for x in candchunks]
+    score=np.array(_flatten([job.get() for job in jobs]))
+    
+    cpop=copy.deepcopy(pop_ini)
+    niter=0
+    stats={'generation':[],
+           'min':[],
+           'max':[],
+           'mean':[],
+           'median':[]}
+    while not stopcrit:
+        # build candidate pop        
+        pop_new=[]
+        for i in range(npop): # population loop
+            a,b,c=tuple(np.random.permutation(np.delete(np.arange(npop), i))[0:3])
+            cand=np.zeros(D, dtype=np.float)
+            j=np.random.randint(D)
+            for k in range(D):
+                if np.random.uniform()<CR or k==D-1:
+                    cand[j]=cpop[c][j]+F*( cpop[a][j] - cpop[b][j])
+                else:
+                    cand[j] = cpop[i][j]
+                j=(j+1) % D
+            pop_new.append(cand)
+
+        # update score
+        candchunks=_cont_chunks(pop_new, ncpu)
+        jobs=[pool.apply_async( _eval_pop, (x, objective,objargs) ) for x in candchunks]
+        new_score=np.array(_flatten([job.get() for job in jobs]), dtype=np.float)
+
+        score=np.where( new_score<=score, new_score, score)
+        cpop=[pop_new[i] if new_score[i]<=score[i] else cpop[i] for i in range(npop)]
+
+        # save stats
+        if save_stats!=None and niter % save_stats == 0:
+            stats['generation'].append(niter)
+            stats['min'].append(np.min(score))
+            stats['max'].append(np.max(score))
+            stats['mean'].append(np.mean(score))
+            stats['median'].append(np.median(score))
+            
+        niter+=1
+        if niter>=gen_max:
+            stopcrit=True
+
+    return cpop, score, stats if save_stats!=None else None
+    
 
 
 
