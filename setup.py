99a9d1745ef2f1e19a92b4660d828d93ad8edbb8
Matthias Mittner <mittner@uva.nl>
2013-09-12 12:01:54 +0200

    trying to change build order in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@
 # ----------- setup.py
 #
 from setuptools import setup, Extension, find_packages
-from setuptools.command.build_py import build_py, install
+from setuptools.command.build import build
 import numpy as np
 import sys, os
+
+MAJOR=0
+MINOR=1
+MICRO=0
+VERSION='%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
 
 def gsl_get_include():
     if sys.platform == "win32":
@@ -40,8 +46,19 @@ def gsl_get_libraries():
 #
 #------------------------------------------------------------------------
 #
+
+class my_build(build):
+    # different order: build_ext *before* build_py, so that
+    # build_py can already use crace.py
+    sub_commands = [('build_ext',     build.build.has_ext_modules),
+                    ('build_py',      build.build.has_pure_modules),
+                    ('build_clib',    build.build.has_c_libraries),
+                    ('build_scripts', build.build.has_scripts),
+                   ]
+
 dist=setup(
     name="pyrace",
+    version=VERSION,
     packages=find_packages(),#["pyrace"],
     author="Matthias Mittner",
     author_email="mittner@uva.nl",
@@ -53,6 +70,7 @@ dist=setup(
                            library_dirs=[gsl_get_library_dir()],
                            swig_opts=['-modern'])],
     py_modules=['crace'],
+    cmdclass = {'build': my_build},
     )
 
 """ solution to issue?
