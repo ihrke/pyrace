e3a063763a18d3b72540f71e7ec571656c4e1610
Matthias Mittner <mittner@uva.nl>
2013-09-12 17:31:31 +0200

    try to commit from travis

---
+++ README.md
@@ -0,0 +1,2 @@
+following 
+http://sleepycoders.blogspot.se/2013/03/sharing-travis-ci-generated-files.html
---
+++ commit_from_travis.sh
@@ -0,0 +1,20 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update \n"
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=notebook_tests https://${GH_TOKEN}@github.com/ihrke/pyrace.git  notebook_tests > /dev/null
+
+  #go into diractory and copy data we're interested in to that directory
+  cd notebook_tests
+  python rerun_nb.py
+
+  #add, commit and push files
+  git commit -a -m "Travis build $TRAVIS_BUILD_NUMBER"
+  git push -fq origin notebook_tests > /dev/null
+  echo -e "Done magic with coverage\n"
+fi
