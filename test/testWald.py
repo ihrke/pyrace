30c110ca9cf91a7eb0a71e520c648053549a7f02
Matthias Mittner <mittner@uva.nl>
2013-09-30 13:44:08 +0200

    Wald with starting point variability WIP
    pdf working + unit-test against Trish van Zandt's R code (code not yet included)

--- test/testWald.py
+++ test/testWald.py
@@ -1,5 +1,5 @@
 from common import *
-from pyrace import ShiftedWaldAccumulator
+from pyrace import ShiftedWaldAccumulator, VarWaldAccumulator
 
 class testWald(PlottingEnabledTestCase):
     def test_wald_acc(self):
@@ -10,7 +10,7 @@ class testWald(PlottingEnabledTestCase):
         self.assertAlmostEqual(0, acc.cdf(0.01))
         assert abs( acc.cdf(10000)-1)<1e-4
 
-    def test_sample(self):
+    def test_wald_sample(self):
         acc=ShiftedWaldAccumulator(.2, .2, 2.0)
         nsamples=100000
         x=np.linspace(0,10, nsamples)
@@ -33,6 +33,91 @@ class testWald(PlottingEnabledTestCase):
             #pl.plot(x,dens(x),    color='green', label='kde')
             pl.legend()
             self.savefig()
-        
+
+
+import rpy2.rinterface as rinterface
+import rpy2.robjects as robjects
+
+## load the R-code by Logan
+r = robjects.r
+r['source']('./misc/UWA.R')
+
+class testVarWald(PlottingEnabledTestCase):
+    def cmp_pdf_against_R(self, pars):
+        """
+        check the plots, too
+        """
+        pars.update({'k':pars['b']-pars['A']/2.0,
+                     'l':pars['v'],
+                     'a':pars['A']/2.0})
+        acc=VarWaldAccumulator( theta=pars['ter'],
+                                gamma=pars['v'],
+                                alpha=pars['b'],
+                                A=pars['A'])
+        nsamples=1000
+        x=np.linspace(0,3,nsamples)
+        rx=rinterface.FloatSexpVector(x - pars['ter'] )
+
+        yr=r['digt'](rx, k=pars['k'], l=pars['l'], a=pars['a'])
+        y=np.array(yr)
+        assert np.all(y-acc.pdf(x) < 1e-10), str(pars)
+
+        pl.clf()
+        pl.plot(x, acc.pdf(x), color='blue', label='python')
+        pl.plot(x, y, color='red', label='R')
+        pl.legend()
+        self.savefig()
+
+
+    def test_varwald_pdf_against_R(self):
+        pars={'A':1.0, 'b':2.0, 'v':.5, 'ter':.1}
+        self.cmp_pdf_against_R(pars)
+
+        pars={'A':1.2, 'b':2.0, 'v':.6, 'ter':.5}
+        self.cmp_pdf_against_R(pars)
+
+        pars={'A':1.2, 'b':2.0, 'v':0, 'ter':.5}
+        self.cmp_pdf_against_R(pars)
+
+    def test_varwald_acc(self):
+        pars={'A':1.0, 'b':2.0, 'v':.5, 'ter':.1}
+        acc=VarWaldAccumulator( theta=pars['ter'],
+                                gamma=pars['v'],
+                                alpha=pars['b'],
+                                A=pars['A'])
+        nsamples=10000
+        x=np.linspace(0,10, nsamples)
+        self.assertAlmostEqual(scipy.integrate.quad(acc.pdf, 0, np.infty)[0], 1)
+        self.assertAlmostEqual(0, acc.cdf(0.01))
+        assert abs( acc.cdf(10000)-1)<1e-4
+
+    def test_varwald_sample(self):
+        pars={'A':1.0, 'b':2.0, 'v':.5, 'ter':.1}
+        acc=VarWaldAccumulator( theta=pars['ter'],
+                                gamma=pars['v'],
+                                alpha=pars['b'],
+                                A=pars['A'])
+        nsamples=100000
+        x=np.linspace(0,10, nsamples)
+
+        import pylab as pl
+        samp=acc.sample(nsamples)
+        #dens=scipy.stats.gaussian_kde(samp[samp<10])
+        #pl.hist(acc.sample(nsamples),200, normed=True)
+        h,hx=np.histogram(samp, density=True, bins=1000)
+        hx=hx[:-1]+(hx[1]-hx[0])/2.
+        assert np.any(np.abs(h-acc.pdf(hx))<0.5)
+
+        if True:
+            #pl.subplot(2,1,1)
+            pl.hist(samp[samp<10],300, normed=True, alpha=.3)
+            pl.xlim(0,3)
+
+            #pl.subplot(2,1,2)
+            pl.plot(x,acc.pdf(x), color='red', label='analytical')
+            #pl.plot(x,dens(x),    color='green', label='kde')
+            pl.legend()
+            self.savefig()
+
 if __name__ == '__main__':
     unittest.main()
